54308726f575425d9bec6f765da0c993c2e220e7
hp0592553-cyber <hp0592553@gmail.com>
2026-02-04 19:28:11 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,15 +39,18 @@ android {
 }
 
 dependencies {
+    // Bibliotecas Base do Android
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
-    // SDKs do Frogobox
+    // O CORAÇÃO DO SAPO (Frogobox SDKs)
+    // Se o build falhar aqui, tente mudar para 2.0.9 ou 2.0.8
     implementation("com.github.frogobox:frogo-sdk:2.1.0")
     implementation("com.github.frogobox:frogo-admob:2.0.5")
     
+    // Testes
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
