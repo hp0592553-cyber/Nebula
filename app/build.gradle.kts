ecfcdac57fb66ba5e1908fbe75caaf297fa373ca
hp0592553-cyber <hp0592553@gmail.com>
2026-02-04 19:17:50 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,102 +1,55 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-
-    namespace = ProjectSetting.PROJECT_NAME_SPACE
-    compileSdk = ProjectSetting.PROJECT_COMPILE_SDK
+    namespace = "com.frogobox.webview"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = ProjectSetting.PROJECT_APP_ID
-        minSdk = ProjectSetting.PROJECT_MIN_SDK
-        targetSdk = ProjectSetting.PROJECT_TARGET_SDK
-        versionCode = ProjectSetting.PROJECT_VERSION_CODE
-        versionName = ProjectSetting.PROJECT_VERSION_NAME
-
-        multiDexEnabled = true
-        vectorDrawables.useSupportLibrary = true
+        applicationId = "com.frogobox.webview"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 18
+        versionName = "18.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        // Naming APK // AAB
-        setProperty("archivesBaseName", "${ProjectSetting.NAME_APK}(${versionName})")
-
-        // Inject app name for debug
-        resValue("string", "app_name", ProjectSetting.nameAppDebug)
-
-        // Inject admob id for debug
-        resValue("string", "admob_app_id", AdmobValue.Debug.APP_ID)
-        resValue("string", "admob_banner", AdmobValue.Debug.BANNER)
-        resValue("string", "admob_interstitial", AdmobValue.Debug.INTERSTITIAL)
-
-    }
-
-
-    signingConfigs {
-        create("release") {
-            // You need to specify either an absolute path or include the
-            // keystore file in the same directory as the build.gradle file.
-            // [PROJECT FOLDER NAME/app/[COPY YOUT KEY STORE] .jks in here
-            storeFile = file(ProjectSetting.PLAYSTORE_STORE_FILE)
-            storePassword = ProjectSetting.PLAYSTORE_STORE_PASSWORD
-            keyAlias = ProjectSetting.PLAYSTORE_KEY_ALIAS
-            keyPassword = ProjectSetting.PLAYSTORE_KEY_PASSWORD
-        }
     }
 
     buildTypes {
-
-        getByName("debug") {
-            applicationIdSuffix = ".dev"
-        }
-
         getByName("release") {
             isMinifyEnabled = false
-
-            // Generated Signed APK / AAB
-            signingConfig = signingConfigs.getByName("release")
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-
-            // Inject app name for release
-            resValue("string", "app_name", ProjectSetting.NAME_APP)
-
-            // Inject admob id for release
-            resValue("string", "admob_app_id", AdmobValue.APP_ID)
-            resValue("string", "admob_banner", AdmobValue.BANNER)
-            resValue("string", "admob_interstitial", AdmobValue.INTERSTITIAL)
-
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    
+    kotlinOptions {
+        jvmTarget = "11"
     }
 
     buildFeatures {
         viewBinding = true
-        buildConfig = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
-        freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
     }
 }
 
 dependencies {
-
-    implementation(libs.frogo.android)
-
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    
+    // SDKs do Frogobox
+    implementation("com.github.frogobox:frogo-sdk:2.1.0")
+    implementation("com.github.frogobox:frogo-admob:2.0.5")
+    
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
