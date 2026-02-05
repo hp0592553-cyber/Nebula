7aacdf63c86ecd4ec455ba35316c0791b9c47608
hp0592553-cyber <hp0592553@gmail.com>
2026-02-05 00:27:45 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,13 +7,22 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    // MUDANÇA MESTRE: PREFER_SETTINGS com o portal escancarado!
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven { 
+            url = uri("https://jitpack.io") 
+            // Truque de Gênio: Ignora restrições de metadados
+            metadataSources {
+                mavenPom()
+                artifact()
+            }
+        }
     }
 }
 
 rootProject.name = "Nebulosa"
 include(":app")
+
