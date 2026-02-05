9e333cb3b856aab05b0481a8354ac53dd9e13b9b
hp0592553-cyber <hp0592553@gmail.com>
2026-02-05 00:25:11 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
     }
 }
 
+// Repositório declarado aqui também como plano B
 repositories {
     google()
     mavenCentral()
@@ -49,11 +50,12 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     
-    // VERSÕES REVISADAS PARA MATAR O ERRO 401
-    implementation("com.github.frogobox:frogo-sdk:2.0.9")
-    implementation("com.github.frogobox:frogo-admob:2.0.4")
+    // USANDO AS VERSÕES MAIS RECENTES QUE O JITPACK COSTUMA LIBERAR
+    implementation("com.github.frogobox:frogo-sdk:2.1.1")
+    implementation("com.github.frogobox:frogo-admob:2.0.9")
     
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
